60ac88699697797025589bf4b5e3a2904066be05
Umut Keskin <ukeskin2626@gmail.com>
2023-02-23 22:33:29 +0300

    fix: script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import "@code-hike/mdx/dist/index.css";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <Script
+        strategy="afterInteractive"
+        data-website-id="6e2d5876-59d9-4702-b422-0791399569c5"
+        src="https://umami-omega-three.vercel.app/umami.js"
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
