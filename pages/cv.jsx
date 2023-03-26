7391753527ac23e248a0add015f66f4e9ac5e9b5
Umut Keskin <ukeskin2626@gmail.com>
2023-03-26 11:58:11 +0300

    Update cv.jsx

--- pages/cv.jsx
+++ pages/cv.jsx
@@ -4,7 +4,7 @@ export default function cv() {
   return (
     <div className="w-full h-screen bg-[#d1d1d1]">
       <iframe
-        src="https://drive.google.com/file/d/1iAJEQpQ0A6rG2f5wDLilKvPiWcs_Z1YL/preview"
+        src="https://drive.google.com/file/d/18syoDRuIEQiJMbovWvPQ_KUZQOt0IYYb/preview"
         width="95%"
         height="100%"
       ></iframe>
